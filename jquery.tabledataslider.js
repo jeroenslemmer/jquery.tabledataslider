9f1e8224ffe3cd2f050b7edc447aa82f383a9619
jeroenslemmer <jeroen.slemmer@gmail.com>
2015-04-11 22:22:49 +0200

    plugin restuctured
    functions within were misplaced

--- jquery.tabledataslider.js
+++ jquery.tabledataslider.js
@@ -58,7 +58,7 @@
 			var detailContainer = $('#'+settings.detailIdPrefix+match[0]);
 			if (detailContainer.length == 0)throw new Error('No TD element found to be a tabledata slider. Check its id.')
 			if (detailContainer.prop('tagName')!='TD') throw new Error('Only TD elements can be a tabledata slider. Element is a '+ detailContainer.prop('tagName')+' element');
-		// if not already slider
+			// if not already slider
 			if ($(detailContainer).find('.tabledata-slider').length == 0){ 
 				var details = $(detailContainer).find('>*');
 				// create nested containers: outer one for manipulating height, inner one for calculating maximum height of its content
@@ -72,48 +72,48 @@
 				});
 				// set initial state as an attribute 'tabledata-slider' to 'closed'
 				$(this).attr('tabledata-slider','closed');
-				
-				// open or close the details for selection
-				$(this).init.prototype.tabledataPerformSlide = function(open){
-					for (var o in this) {
-						if (typeof this[o] == 'object')
-							$(this[o]).tabledataPerformSlidePrivate(open);
-					}
-					return this;
-				}
-					
-				// open or close the details one element
-				$(this).init.prototype.tabledataPerformSlidePrivate = function(open){
-					var me = this[0];
-					if ($(me).attr('tabledata-slider') == 'opening' || $(me).attr('tabledata-slider') == 'closing') return;
-					var height = $(me.wrapper).height();
-					if (open){
-						$(me).attr('tabledata-slider','opening');
-						var duration = (settings.openRelativeDuration>0)?(height-settings.detailMinHeight)*settings.openRelativeDuration:settings.openFixedDuration;
-						$(me.slider).animate({height:height},duration,function(){
-							$(me).attr('tabledata-slider','opened');
-							settings.afteropening();
-						});
-					} else {
-						$(me).attr('tabledata-slider','closing');
-						var duration = (settings.closeRelativeDuration>0)?(height-settings.detailMinHeight)*settings.closeRelativeDuration:settings.closeFixedDuration;
-						$(me.slider).animate({height:settings.detailMinHeight},duration,function(){
-							$(me).attr('tabledata-slider','closed');
-							settings.afterclosing();
-						});
-					}
-					return this;
-				}
-				
-				// bind opening and closing to event
-				$(this).bind(settings.toggleEvent,function(){
-					if ($(this).attr('tabledata-slider') == 'closed')
-						$(this).tabledataPerformSlidePrivate(true);
-					else if ($(this).attr('tabledata-slider') == 'opened')
-						$(this).tabledataPerformSlidePrivate(false);
-				});	
 			}
 		});
+		
+		// open or close the details for selection
+		$(this).init.prototype.tabledataPerformSlide = function(open){
+			for (var o in this) {
+				if (typeof this[o] == 'object')
+					$(this[o]).tabledataPerformSlidePrivate(open);
+			}
+			return this;
+		}
+					
+		// open or close the details one element
+		$(this).init.prototype.tabledataPerformSlidePrivate = function(open){
+			var me = this[0];
+			if ($(me).attr('tabledata-slider') == 'opening' || $(me).attr('tabledata-slider') == 'closing') return;
+			var height = $(me.wrapper).height();
+			if (open){
+				$(me).attr('tabledata-slider','opening');
+				var duration = (settings.openRelativeDuration>0)?(height-settings.detailMinHeight)*settings.openRelativeDuration:settings.openFixedDuration;
+				$(me.slider).animate({height:height},duration,function(){
+					$(me).attr('tabledata-slider','opened');
+					settings.afteropening();
+				});
+			} else {
+				$(me).attr('tabledata-slider','closing');
+				var duration = (settings.closeRelativeDuration>0)?(height-settings.detailMinHeight)*settings.closeRelativeDuration:settings.closeFixedDuration;
+				$(me.slider).animate({height:settings.detailMinHeight},duration,function(){
+					$(me).attr('tabledata-slider','closed');
+					settings.afterclosing();
+				});
+			}
+			return this;
+		}
+				
+		// bind opening and closing to event
+		$(this).bind(settings.toggleEvent,function(){
+			if ($(this).attr('tabledata-slider') == 'closed')
+				$(this).tabledataPerformSlidePrivate(true);
+			else if ($(this).attr('tabledata-slider') == 'opened')
+				$(this).tabledataPerformSlidePrivate(false);
+		});	
 		return this;
 	};
 }( jQuery ));
